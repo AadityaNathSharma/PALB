a175425a53f430aa2fc314cf0da142a6389dc308
AadityaNathSharma <mr.aadityanathsharma@gmail.com>
2026-03-20 09:49:35 +0530

    Delete Exp-1 directory

--- Exp-1/04 code.py
+++
@@ -1,8 +0,0 @@
-class Solution:    
-    def findUnion(self, a, b):
-        result = set(a)   # add all elements of a
-        
-        for num in b:
-            result.add(num)
-        
-        return list(result)
--- Exp-1/05 code.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def largest(self, arr):
-        max_val = arr[0]
-        
-        for num in arr:
-            if num > max_val:
-                max_val = num
-        
-        return max_val
